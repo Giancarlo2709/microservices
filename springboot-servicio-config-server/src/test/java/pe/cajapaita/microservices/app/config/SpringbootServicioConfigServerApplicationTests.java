c774a3c91f94ea0dee23d2eb15f635484b88a31c
Giancarlo2709 <giancarlo2709@gmail.com>
2020-01-24 15:09:57 -0500

    Config Server

---
+++ springboot-servicio-config-server/src/test/java/pe/cajapaita/microservices/app/config/SpringbootServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cajapaita.microservices.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
